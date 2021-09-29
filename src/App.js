f2cfd217caea0f8a84ffe0d8dc43845a12e228bb
Donovan Cain Monteiro <88496063+don0716@users.noreply.github.com>
2021-09-30 01:21:55 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ var emojiDictionary = {
   "😘": "kiss"
 };
 
+var emojisWeKnow = Object.keys(emojiDictionary);
+
 export default function App() {
   var [meaning, setMeaning] = useState("");
 
@@ -22,17 +24,37 @@ export default function App() {
     var userInput = emoji.target.value;
     var meaning = emojiDictionary[userInput];
 
-    if (meaning == undefined) {
+    if (meaning === undefined) {
       meaning = "We do not have this in our database";
     }
     setMeaning(meaning);
   }
 
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
+
   return (
     <div className="App">
-      <h1>inside outt!</h1>
+      <h1>Emoji Interpreter!</h1>
       <input onChange={changeEmojiHandler}></input>
-      <div>Meaning: {meaning}</div>
+      <div>{meaning}</div>
+
+      <div> Emojis We Know! </div>
+
+      {emojisWeKnow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ cursor: "pointer", padding: "0.5rem", fontSize: "1rem" }}
+            key={emoji}
+          >
+            {" "}
+            {emoji}
+          </span>
+        );
+      })}
     </div>
   );
 }
